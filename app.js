7dcc9446cd385eee663ddcc839f3f2099006e076
Jacob <jacobdeansd@gmail.com>
2020-05-12 23:02:12 -0700

    custom skycons

--- app.js
+++ app.js
@@ -1,29 +1,57 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
-    let tempDescription = document.querySelector('.temoerature-description');
-    let tempDegree = document.querySelector('.temoerature-degree');
-    let locationTimezone = document.querySelector('.location-timezone');
+    let tempDescription = document.querySelector('.temperature-description');
+    let tempDegree = document.querySelector('.temperature-degree');
+    let locationCity = document.querySelector('.location-city');
+    let temperatureSection = document.querySelector(".temperature");
+    const temperatureSpan = document.querySelector(".temperature span");
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const apiKey = '970f2cb858bf46eadcaf13f9d6b65996';
+            const apiKey = '68038ea0890a4308ac2070cba884d1fb';
             const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+            const api = `${proxy}https://api.weatherbit.io/v2.0/current?&lat=${lat}&lon=${long}&units=I&key=${apiKey}`;
 
             fetch(api)
                 .then(res => {
                     return res.json();
                 })
-                .then(data => {
-                    console.log(data);
-                    const temp = data.main.temp;
-                    const {description} = data.weather[0];
-                    tempDegree.textContent = (parseFloat(temp)-273.15)*9/5+32;
+                .then(data2 => {
+                    const temp = data2.data[0].temp;
+                    const description = data2.data[0].weather.description;
+                    const city = data2.data[0].city_name;
+                
+                    tempDegree.textContent = temp;
+                    tempDescription.textContent = description;
+                    locationCity.textContent = city;
+
+                    let celsius = (temp - 32) * (5 / 9);
+
+                    setIcons(description, document.querySelector('.icon'));
+
+                    temperatureSection.addEventListener("click", () => {
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            tempDegree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpan.textContent = "F";
+                            tempDegree.textContent = temp;
+                        }
+                        });
+                    });
                 });
-        });
+        
     }
+
+    setIcons = (icon, iconID) => {
+        const skycons = new Skycons({color: 'white'});
+        const currentIcon = icon.replace(/\ /g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+
 });
